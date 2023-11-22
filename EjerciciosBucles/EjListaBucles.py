656dddd7d7c7634dc53b0bd6845de5814f639630
Cristian <cristian.perez.mail@gmail.com>
2023-11-22 10:58:31 +0100

    EjerciciosBucles

---
+++ EjerciciosBucles/EjListaBucles.py
@@ -0,0 +1,10 @@
+﻿#Ej01: Pregunta 4 números al usuario y los va introduciendo en una lista. Al final, los muestra todos.
+
+lista=[None] * 4
+for n in range(0,4):
+    print("Introduce un número:")
+    lista[n]=int(input())
+print(lista)
+
+#Ej02: Similar al anterior, pero se introducen números hasta que el usuario escribe un 0.
+
